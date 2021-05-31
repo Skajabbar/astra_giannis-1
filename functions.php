a709a6a16a400d3a533a2540eee6cfd83d4b6512
Skajabbar <kajabbar@gmail.com>
2021-05-31 20:07:41 +0300

    Updated Giannisduke files

--- functions.php
+++ functions.php
@@ -27,43 +27,10 @@ function child_enqueue_styles() {
 	//με αυτό φορτώνει το fonts.css
 	wp_register_style( 'giannis_fonts_css', get_stylesheet_directory_uri() . '/css/fonts.css');
 	wp_enqueue_style( 'giannis_fonts_css' );
+
+	//με αυτό φορτώνει το https://github.com/scroll-out/scroll-out.github.io
+	wp_register_script( 'scroll-out', get_stylesheet_directory_uri() . '/js/scroll-out.min.js' , '', '', true );
+	wp_enqueue_script( 'scroll-out' );
 }
 
 add_action( 'wp_enqueue_scripts', 'child_enqueue_styles', 15 );
-
-function my_before_shop_loop() { ?>
-	<p class="test custom-test">
-		my_before_shop_loop
-	</p>
-<?php }
-add_action ('woocommerce_before_shop_loop', 'my_before_shop_loop', 5 );
-
-
-function my_footer() { ?>
-		<div class=" test custom-test">
-			My footer
-		</div>
-<?php }
-add_action ('woocommerce_after_shop_loop', 'my_footer', 5 );
-
-
-function giannis_test1() {
-		 echo "<div class='test custom-test'>";
-		 echo "Giannis Test Output";
-		 echo "</div>";
-}
-add_action( 'giannis_test', 'giannis_test1', 5 );
-
-function giannis_test2() { ?>
-		 <div class='test custom-test'>
-			 GiannisTest 2
-		 </div>
-<?php }
-add_action( 'giannis_test', 'giannis_test2', 10 );
-
-function giannis_test3() {
-		 echo "<div class='test custom-test'>";
-     echo "Giannis Test Output 3";
-		 echo "</div>";
-}
-add_action( 'giannis_test', 'giannis_test3', 15 );
